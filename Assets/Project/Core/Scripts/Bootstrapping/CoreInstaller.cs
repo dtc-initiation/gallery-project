cb8393e4681189e1bb6b009a3170ef0b7c567cc2
JiMu_Kang <dtc.initiation@gmail.com>
2025-10-17 19:23:04 +0900

    [O] Plugins

---
+++ Assets/Project/Core/Scripts/Bootstrapping/CoreInstaller.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Project.Core.Scripts.Bootstrapping {
+    public class CoreInstaller : MonoInstaller {
+        public override void InstallBindings() {
+            Debug.Log("CoreInstaller: Installing Core");
+        }
+    }
+}
