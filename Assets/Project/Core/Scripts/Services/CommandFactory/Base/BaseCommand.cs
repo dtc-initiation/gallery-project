b89cbea480fda68a4310b94606830c762c0e1a3d
JiMu_Kang <dtc.initiation@gmail.com>
2025-10-21 17:55:12 +0900

    [F] Command Factory

---
+++ Assets/Project/Core/Scripts/Services/CommandFactory/Base/BaseCommand.cs
@@ -0,0 +1,13 @@
+﻿using Zenject;
+
+namespace Project.Core.Scripts.Services.CommandFactory.Base {
+    public abstract class BaseCommand : IBaseCommand {
+        private DiContainer _diContainer;
+        
+        public void SetObjectResolver(DiContainer diContainer) {
+            _diContainer = diContainer;
+        }
+
+        public abstract void ResolveDependencies();
+    }
+}
