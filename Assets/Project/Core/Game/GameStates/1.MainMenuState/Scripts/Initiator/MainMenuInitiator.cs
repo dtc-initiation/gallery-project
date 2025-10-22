4c9aacd6c94aa8908d03ca0eb8317ee47490b9e8
JiMu_Kang <dtc.initiation@gmail.com>
2025-10-22 14:11:45 +0900

    [F] Main Menu Initiator

---
+++ Assets/Project/Core/Game/GameStates/1.MainMenuState/Scripts/Initiator/MainMenuInitiator.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using Project.Core.Scripts.Services.ApplicationStateMachine.Base;
+using Project.Core.Scripts.Services.InitiatorService.Base;
+using Project.Core.Scripts.Services.Logger.Base;
+using Project.Core.Scripts.Services.SceneService.Base;
+using Project.Core.Scripts.Utils;
+using UnityEngine;
+using Zenject;
+
+namespace Project.Core.Game.GameStates._1.MainMenuState.Scripts.Initiator {
+    public class MainMenuInitiator : ISceneInitiator, IMainMenuInitiator {
+        public string SceneName => "MainMenu";
+
+        [Inject]
+        public MainMenuInitiator(IApplicationStateService applicationStateService) {}
+        
+        public Awaitable LoadEntryPoint(CancellationTokenSource cancellationTokenSource) {
+            return AwaitableUtils.CompletedTask;
+        }
+
+        public Awaitable StartEntryPoint(CancellationTokenSource cancellationTokenSource) {
+            return AwaitableUtils.CompletedTask;
+        }
+
+        public Awaitable UnloadExitPoint(CancellationTokenSource cancellationTokenSource) {
+            return AwaitableUtils.CompletedTask;
+        }
+    }
+}
---
+++ Assets/Project/Core/Game/GameStates/1.MainMenuState/Scripts/Initiator/MainMenuInstaller.cs
@@ -0,0 +1,9 @@
+﻿using Zenject;
+
+namespace Project.Core.Game.GameStates._1.MainMenuState.Scripts.Initiator {
+    public class MainMenuInstaller : MonoInstaller {
+        public override void InstallBindings() {
+            Container.Bind<IMainMenuInitiator>().To<MainMenuInitiator>().AsSingle().NonLazy();
+        }
+    }
+}
