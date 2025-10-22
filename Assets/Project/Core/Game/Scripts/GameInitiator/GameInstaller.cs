071227272df272ddf4d53c152a80e0e79677f3e8
JiMu_Kang <dtc.initiation@gmail.com>
2025-10-22 10:24:54 +0900

    [O] Bindings modification

--- Assets/Project/Core/Game/Scripts/GameInitiator/GameInstaller.cs
+++ Assets/Project/Core/Game/Scripts/GameInitiator/GameInstaller.cs
@@ -3,10 +3,9 @@ using Zenject;
 
 namespace Project.Core.Game.Scripts.GameInitiator {
     public class GameInstaller : MonoInstaller{
-        [SerializeField] private IGameInitiator _gameInitiator;
         
         public override void InstallBindings() {
-            Container.Bind<IGameInitiator>().FromInstance(_gameInitiator).AsSingle().NonLazy();
+            Container.Bind<IGameInitiator>().To<GameInitiator>().AsSingle().NonLazy();
         }
     }
 }
