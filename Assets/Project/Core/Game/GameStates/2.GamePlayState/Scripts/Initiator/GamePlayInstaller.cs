e4cf2f7775d930808f1062465ab25ae99c389f6a
JiMu_Kang <dtc.initiation@gmail.com>
2025-10-28 16:21:21 +0900

    [F] Vanilla GamePlayInitiation loop

---
+++ Assets/Project/Core/Game/GameStates/2.GamePlayState/Scripts/Initiator/GamePlayInstaller.cs
@@ -0,0 +1,9 @@
+﻿using Zenject;
+
+namespace Project.Core.Game.GameStates._2.GamePlayState.Scripts.Initiator {
+    public class GamePlayInstaller : MonoInstaller {
+        public override void InstallBindings() {
+            Container.Bind<IGamePlayInitiator>().To<GamePlayInitiator>().AsSingle().NonLazy();
+        }
+    }
+}
