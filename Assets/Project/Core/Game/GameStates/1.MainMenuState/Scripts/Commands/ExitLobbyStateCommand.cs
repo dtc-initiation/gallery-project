c5e7a301fae030e1ea8efb168936103bf9aa4d7d
JiMu_Kang <dtc.initiation@gmail.com>
2025-10-28 11:58:43 +0900

    [F] Enter/Exit lobbystate command

---
+++ Assets/Project/Core/Game/GameStates/1.MainMenuState/Scripts/Commands/ExitLobbyStateCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using Project.Core.Game.GameStates._1.MainMenuState.Scripts.Mvc;
+using Project.Core.Scripts.Services.CommandFactory.Base;
+using Project.Core.Scripts.Utils;
+using UnityEngine;
+
+namespace Project.Core.Game.GameStates._1.MainMenuState.Scripts.Commands {
+    public class ExitLobbyStateCommand : BaseCommand, ICommandAsyncVoid {
+        private ILobbyButtonsController _lobbyButtonsController;
+        
+        public override void ResolveDependencies() {
+            _lobbyButtonsController = _diContainer.Resolve<ILobbyButtonsController>();
+        }
+
+        public Awaitable Execute(CancellationTokenSource cancellationTokenSource) {
+            _lobbyButtonsController.Dispose();
+            return AwaitableUtils.CompletedTask;
+        }
+    }
+}
