d58566c0d1d66278d58525d5b14eba7136fc594a
JiMu_Kang <dtc.initiation@gmail.com>
2025-10-29 15:07:59 +0900

    [O] Playtesting OnScreenControllers

--- Assets/Project/Core/Game/GameStates/2.GamePlayState/Runtime/Scripts/Initiator/IGamePlayInitiator.cs
+++ Assets/Project/Core/Game/GameStates/2.GamePlayState/Runtime/Scripts/Initiator/IGamePlayInitiator.cs
@@ -1,6 +1,6 @@
 ﻿using Project.Core.Scripts.Services.InitiatorService.Base;
 
-namespace Project.Core.Game.GameStates._2.GamePlayState.Scripts.Initiator {
+namespace Project.Core.Game.GameStates._2.GamePlayState.Runtime.Scripts.Initiator {
     public interface IGamePlayInitiator : ISceneInitiator {
         
     }
---
+++ Assets/Project/Core/Game/GameStates/2.GamePlayState/Runtime/Scripts/PlayTesting/OnScreenControlsView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Project.Core.Game.GameStates._2.GamePlayState.Runtime.Scripts.PlayTesting {
+    public class OnScreenControlsView : MonoBehaviour {
+        [SerializeField] private Canvas onScreenControlCanvas;
+
+        public void InitializeEntryPoint(Camera uiCamera) {
+            onScreenControlCanvas.worldCamera = uiCamera;
+            onScreenControlCanvas.enabled = true;
+        }
+
+        public void InitializeExitPoint() {
+            onScreenControlCanvas.enabled = false;
+            onScreenControlCanvas.worldCamera = null;
+        }
+    }
+}
+
