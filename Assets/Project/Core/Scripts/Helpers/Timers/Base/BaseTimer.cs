386ecf2dbaa35ded68d3a63a6693df160a4cc156
JiMu_Kang <dtc.initiation@gmail.com>
2025-10-30 19:48:00 +0900

    [F] Base and Countdown timer

---
+++ Assets/Project/Core/Scripts/Helpers/Timers/Base/BaseTimer.cs
@@ -0,0 +1,29 @@
+﻿namespace Project.Core.Scripts.Helpers.Timers.Base {
+    public abstract class BaseTimer : ITimer {
+        protected float InitialTime;
+        protected float CurrentTime { get; set; }
+        public bool IsRunning { get; private set; }
+
+        public float Progress => CurrentTime / InitialTime;
+        
+        protected BaseTimer(float initialTime) {
+            InitialTime = initialTime;
+            IsRunning = false;
+        }
+        
+        public void StartTimer() {
+            CurrentTime = InitialTime;
+            if (!IsRunning) {
+                IsRunning = true;
+            }
+        }
+
+        public void StopTimer() {
+            if (IsRunning) {
+                IsRunning = false;
+            }
+        }
+
+        public abstract bool Tick(float deltaTime);
+    }
+}
---
+++ Assets/Project/Core/Scripts/Helpers/Timers/CountDownTimer.cs
@@ -0,0 +1,22 @@
+﻿using Project.Core.Scripts.Helpers.Timers.Base;
+
+namespace Project.Core.Scripts.Helpers.Timers {
+    public class CountDownTimer : BaseTimer{
+        public CountDownTimer(float initialTime) : base(initialTime) {}
+
+        public override bool Tick(float deltaTime) {
+            if (IsRunning & CurrentTime > 0) {
+                CurrentTime -= deltaTime;
+            }
+            if (IsRunning & CurrentTime <= 0) {
+                StopTimer();
+                return true;
+            }
+            return false;
+        }
+
+        public void ResetTimer() {
+            CurrentTime = InitialTime;
+        }
+    }
+}
