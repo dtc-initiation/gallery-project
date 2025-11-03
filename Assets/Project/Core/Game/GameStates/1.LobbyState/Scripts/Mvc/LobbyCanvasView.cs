acb31de67f621c45f72bed119e422932435e9922
JiMu_Kang <dtc.initiation@gmail.com>
2025-11-03 13:44:30 +0900

    [O] Lobby canvas ui plane distance

--- Assets/Project/Core/Game/GameStates/1.LobbyState/Scripts/Mvc/LobbyCanvasView.cs
+++ Assets/Project/Core/Game/GameStates/1.LobbyState/Scripts/Mvc/LobbyCanvasView.cs
@@ -3,9 +3,19 @@
 namespace Project.Core.Game.GameStates._1.LobbyState.Scripts.Mvc {
     public class LobbyCanvasView : MonoBehaviour {
         [SerializeField] private Canvas lobbyCanvas;
+        [SerializeField] private float planeDistance;
 
         public void InitializeEntryPoint(Camera uiCamera) {
+            lobbyCanvas.renderMode = RenderMode.ScreenSpaceCamera;
             lobbyCanvas.worldCamera = uiCamera;
+            lobbyCanvas.planeDistance = planeDistance;
+            lobbyCanvas.enabled = true;
         }
+
+        public void InitializeExit() {
+            lobbyCanvas.enabled = false;
+            lobbyCanvas.worldCamera = null;
+        }
+        
     }
 }
